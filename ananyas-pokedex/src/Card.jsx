031189369feb68f7a3cad586952ffc12fce6e340
Ananya Singh <139885386+ananyasingh8@users.noreply.github.com>
2024-08-28 12:00:09 -0700

    search text state added

--- ananyas-pokedex/src/Card.jsx
+++ ananyas-pokedex/src/Card.jsx
@@ -1,11 +1,23 @@
+import React from 'react';
 import game from './assets/game.png'
 import './Card.css';
+import search from './assets/search.svg'
 
 export default function Card() {
-    return (
 
+    const [searchtext, setSearchText] = React.useState("");
+
+    function handleChange(event) {
+        setSearchText(event.target.value);
+    }
+
+    return (
         <div className="card">
-            <input type="text" placeholder="Enter Pokemon Name" className="poke-search" />
+            <form className="card-form">
+                <button className="search-icon"><img src={search}/></button>
+                <input type="text" placeholder="Enter Pokemon Name" className="poke-search"
+                onChange={handleChange} />
+            </form>
             <div className="card-content">
                 <img className="card__img" src={game} />
                 <div className="card__text">
